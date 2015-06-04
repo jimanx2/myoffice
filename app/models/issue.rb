201b7a7615a374bdbdd77a0f4680c0abdba5fd29
iMac21 <shafiqaziz06@gmail.com>
2015-06-04 15:24:19 +0800

    update relation and table claim add column approvalstat_id

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,5 @@
 class Issue < ActiveRecord::Base
-    belongs_to :employee
+    belongs_to :assignee, class_name: "Employee", foreign_key: "assign_id"
+    belongs_to :issuer, class_name: "Employee", foreign_key: "issueby_id"
     belongs_to :issuestat
 end
