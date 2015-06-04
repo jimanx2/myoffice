161fc42a21ca59b7411d55b90e73a997d348576a
sazwea <sazweerhapizz@gmail.com>
2015-06-04 14:41:43 +0800

    HRM-position form

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,57 @@
 class PositionsController < ApplicationController
+    
+    
+    def index
+        @position = Position.all
+        
+        respond_to do |format|
+            format.html do
+            end
+            format.json do 
+                render json: @postion
+            end
+            
+        end 
+        
+    end
+        def new
+        end
+        def create
+            @position = Position.new
+            @position.name = params[:name]
+            @position.code = params[:code]
+            @position.eligibilityleave = params[:eligibilityleave]
+            
+            if @position.save!
+                
+                flash[:info] = "Position Record HasBeen Created"
+            redirect_to '/positions'
+            end
+        end
+    def show 
+        @position = Position.find(params[:id])
+    end
+    def edit
+        @position = Position.find(params[:id])
+    end
+    def update
+        @position = Position.find(params[:id])
+        @position.name = params[:name]
+        @position.code = params[:code]
+        @position.eligibilityleave = params[:eligibilityleave]
+        if @position.save!
+            
+            flash[:info] = "Position Record Has Been Updated"
+            redirect_to '/positions'
+        end
+    end
+    
+    def destroy 
+        @position = Position.find(params[:id])
+        if @position.destroy
+            flash[:info] = "Position Record Has Been Destroyed"
+            redirect_to '/positions'
+        end 
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :issues
   resources :employees
   resources :departments
+  resources :positions
     
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
