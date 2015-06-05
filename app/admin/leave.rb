dcb787d65ba0d61b680b48a641a089332cbc5baa
iMac21 <shafiqaziz06@gmail.com>
2015-06-05 10:08:57 +0800

    active_admin fixed

--- app/admin/leave.rb
+++ app/admin/leave.rb
@@ -1,9 +1,9 @@
-ActiveAdmin.register Leave do
+ActiveAdmin.register Leafe do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+    permit_params :id, :employee_id, :startdate, :enddate, :remark, :approveby_id, :approvalstat_id, :leavetype_id, :created_at, :updated_at
 #
 # or
 #
