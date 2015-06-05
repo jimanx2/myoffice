7e71abf0f3b7cc02db14d70edcf3d2201f5ee7a9
iMac21 <shafiqaziz06@gmail.com>
2015-06-05 11:24:46 +0800

    active_admin fixed

---
+++ app/admin/claim.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Claim do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+    permit_params :id, :claimtype_id, :remark, :created_at, :updated_at, :totalclaim, :employee_id,
+    :approvalstat_id
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end
