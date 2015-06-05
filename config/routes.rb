9ee08740402738eee579c6e8d110c126d437e65a
iMac8 <imac8@iMac8s-iMac.local>
2015-06-05 09:42:10 +0800

    latest route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do  
-Rails.application.routes.draw do
+  devise_for :users,:controllers => {
+    :sessions => "sessions"
+  }
 
-  devise_for :users
-  #devise_for :users
-  
   ActiveAdmin.routes(self)
   resources :pages
   root to: 'administrator#index'
-  
   resources :issues
   post 'issues/newissue' => 'issues#create'
   post 'issues/:id/edit' => 'issues#update'
@@ -48,7 +46,6 @@ Rails.application.routes.draw do
     
 
   resources "contacts", only: [:new, :create]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -105,4 +102,4 @@ Rails.application.routes.draw do
   #   end
 end
 
-end
+
