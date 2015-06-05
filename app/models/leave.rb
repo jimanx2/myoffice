52154f0a01ff947b1bb9b2def339e45a8894aec7
iMac14 <imac14@iMac14s-iMac.local>
2015-06-05 09:21:52 +0800

    testing

--- app/models/leave.rb
+++
@@ -1,6 +0,0 @@
-class Leave < ActiveRecord::Base
-    belongs_to :employee
-    belongs_to :approver, class_name: "Employee", foreign_key: "approveby_id"
-    belongs_to :approvalstat
-    belongs_to :leavetype
-end
