b822563f2db420d0aacb0235f46397728a42b2f6
Allison Burger <kluarmen@gmail.com>
2015-06-05 15:26:14 +0800

    Tukar comment database to ticketcomment, run db:migrate

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,5 +2,5 @@ class Issue < ActiveRecord::Base
     belongs_to :assignee, class_name: "Employee", foreign_key: "assign_id"
     belongs_to :issuer, class_name: "Employee", foreign_key: "issueby_id"
     belongs_to :issuestat
-  has_many :comments
+  has_many :ticketcomments
 end
